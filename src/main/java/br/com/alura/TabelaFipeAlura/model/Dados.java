f5e7cf0dc5ca1eb947fd2d3c308a53a1cb13fd94
DavidT <davidt.info@gmail.com>
2024-10-21 20:38:59 -0300

    Modelando as classes da aplicação

---
+++ src/main/java/br/com/alura/TabelaFipeAlura/model/Dados.java
@@ -0,0 +1,4 @@
+package br.com.alura.TabelaFipeAlura.model;
+
+public record Dados (String codigo, String nome){
+}
--- src/main/java/br/com/alura/TabelaFipeAlura/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipeAlura/principal/Principal.java
@@ -1,12 +1,17 @@
 package br.com.alura.TabelaFipeAlura.principal;
 
+import br.com.alura.TabelaFipeAlura.model.Dados;
+import br.com.alura.TabelaFipeAlura.model.Modelos;
 import br.com.alura.TabelaFipeAlura.service.ConsumoAPI;
+import br.com.alura.TabelaFipeAlura.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
     Scanner leitura = new Scanner(System.in);
     private ConsumoAPI consumo = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
 
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
@@ -34,5 +39,21 @@ public class Principal {
 
         var json = consumo.obterDados(endereco);
         System.out.println(json);
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para consulta");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("Modelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
     }
 }
